a38ea6978631e98c602bd6c45624c3ca1512bb21
CunXie <544842679@qq.com>
2019-12-18 15:11:12 +0800

    添加免费版API_KEY、API_SECRET

--- faceppdemo/src/main/java/com/facepp/demo/util/Util.java
+++ faceppdemo/src/main/java/com/facepp/demo/util/Util.java
@@ -3,8 +3,8 @@ package com.facepp.demo.util;
 public class Util {
 
 	//在这边填写 API_KEY 和 API_SECRET
-	public static String API_KEY = "";
-	public static String API_SECRET = "";
+	public static String API_KEY = "EklfiQvn_AnuWIZ91kZF__SbfZiElW2S";
+	public static String API_SECRET = "A_Qq8SoH6I2w6vmbe8pbpXp_oHeDbkaY";
 
 	public static String CN_LICENSE_URL = "https://api-cn.faceplusplus.com/sdk/v3/auth";
 	public static String US_LICENSE_URL = "https://api-us.faceplusplus.com/sdk/v3/auth";
